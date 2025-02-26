bca6e593042ec688ead06cb10bb7952d2d3380fc
anatoliy-katelnitskiy <anatoliy-katelnitskiy@fusion-tech.ru>
2025-02-26 17:09:03 +0300

    feat: added deep linking dor ios

--- App.tsx
+++ App.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
+import { NavigationContainer} from '@react-navigation/native';
 import {RootNavigation} from './src/navigation/rootNaigation';
 import {BootSplashLayout} from './src/components';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import {Alert} from 'react-native';
+
+const linking = {
+  prefixes: ['pkwy://'],
+  getStateFromPath: (path: string) => {
+    if (path) {
+      Alert.alert('', path);
+    }
+  },
+};
 
 export default function App() {
   return (
     <BootSplashLayout>
       <GestureHandlerRootView>
-        <NavigationContainer>
+        <NavigationContainer linking={linking}>
           <RootNavigation />
         </NavigationContainer>
       </GestureHandlerRootView>
--- ios/pkwy/AppDelegate.swift
+++ ios/pkwy/AppDelegate.swift
@@ -14,12 +14,18 @@ class AppDelegate: RCTAppDelegate {
   override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
     self.moduleName = "pkwy"
     self.dependencyProvider = RCTAppDependencyProvider()
-
-    // You can add your custom initial props in the dictionary below.
-    // They will be passed down to the ViewController used by React Native.
     self.initialProps = [:]
 
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  override func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+    return RCTLinkingManager.application(app, open: url, options: options)
+  }
+
+  override func application(_ application: UIApplication, continue userActivity: NSUserActivity,
+                            restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
+    return RCTLinkingManager.application(application, continue: userActivity, restorationHandler: restorationHandler)
   }
 
   override func sourceURL(for bridge: RCTBridge) -> URL? {
