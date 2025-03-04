468d2b48efa4b4925ea411dc7e16bde80722989a
anatoliy-katelnitskiy <anatoliy-katelnitskiy@fusion-tech.ru>
2025-03-04 18:19:25 +0300

    x

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -1,10 +1,11 @@
 /**
  * @format
  */
-
 import React from 'react';
 import ReactTestRenderer from 'react-test-renderer';
 import App from '../App';
+import 'react-native-gesture-handler/jestSetup';
+
 
 test('renders correctly', async () => {
   await ReactTestRenderer.act(() => {
--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,18 @@
 module.exports = {
   preset: 'react-native',
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native|' +
+      '@react-native|' +
+      '@react-navigation|' +
+      'react-native-bootsplash|' +
+      'react-native-fast-secure-storage|' +
+      'react-native-gesture-handler|' +
+      'react-native-maps|' +
+      'react-native-reanimated|' +
+      'react-native-reanimated-carousel|' +
+      'react-native-safe-area-context|' +
+      'react-native-screens|' +
+      'react-native-video' +
+      ')/)',
+  ]
 };
